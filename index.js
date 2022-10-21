d393be00bfa6a8987f5933da072db1f453584e03
AssetGrav <417051@mail.ru>
2022-10-21 20:08:46 +0600

    add token

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const TelegramBot = require('node-telegram-bot-api');
 const express = require('express');
 const cors = require('cors');
 
-const token = "5757177961:AAHDXyhW_5W5qQ8heAkDtA4-BShXWve5lGk"
+const token = os.environ['token']
+console.log("t", token)
 const webAppUrl = "https://singular-cocada-4e7959.netlify.app"
 
 const bot = new TelegramBot(token, {polling: true});
